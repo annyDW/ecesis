278e37f51b4e32bc19323d239e61f4143467e137
luis cataño <luchoad17@gmail.com>
2011-05-21 11:48:48 -0500

    Correccion Css

--- application/controllers/SesionController.php
+++ application/controllers/SesionController.php
@@ -1,6 +1,7 @@
 <?php
 $rootPath = dirname(dirname(__FILE__));
 set_include_path(get_include_path() . PATH_SEPARATOR . $rootPath . PATH_SEPARATOR);
+
 include_once "models/databaseConnect.php";
 class SesionController{
 private $_instancia;
